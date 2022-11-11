bed0f37dba20d62f889de0a4d148c4dbfe02365a
woodashton <116568322+woodashton@users.noreply.github.com>
2022-11-11 11:46:40 -0500

    Update unlockAllParts.js

--- js/other/unlockAllParts.js
+++ js/other/unlockAllParts.js
@@ -1,5 +1,5 @@
 (() => {
-	const state = Object.values(document.querySelector("#app > div > div > div:nth-child(9)"))[1].children[1]._owner.stateNode;
+	const state = Object.values(document.querySelector("#app > div > div > div:nth-child(0)"))[0].children[0]._owner.stateNode;
     	Object.keys(state.state.unlocks).forEach((u)=>{
         	state.state.unlocks[u] = (new Array(101)).fill(null).map((x, i)=>i);
     	});
